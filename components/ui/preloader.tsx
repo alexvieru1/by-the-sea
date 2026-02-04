7ddf83836056c528c70e2d7b5dba36b7e22564cb
Alex <vieru.alexandruu@gmail.com>
2026-02-04 12:30:06 +0200

    customized preloader

--- components/ui/preloader.tsx
+++ components/ui/preloader.tsx
@@ -1,20 +1,14 @@
 'use client';
 
+import { SpinningText } from "./spinning-text";
+
 export default function Preloader() {
   return (
     <div
       id="preloader"
-      className="fixed inset-0 bg-black z-[100] flex items-center justify-center opacity-100 transition-opacity duration-500"
+      className="fixed inset-0 bg-gray-50 z-[100] flex items-center justify-center opacity-100 transition-opacity duration-500"
     >
-      <div className="relative w-16 h-16">
-        {/* Outer rotating circle */}
-        <div className="absolute inset-0 border-4 border-transparent border-t-white border-r-white rounded-full animate-spin" />
-        
-        {/* Inner pulsing dot */}
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="w-3 h-3 bg-white rounded-full animate-pulse" />
-        </div>
-      </div>
+      <SpinningText>VRΛJΛ MΛRII • by the Sea • Health Oasis •</SpinningText>
     </div>
   );
 }
